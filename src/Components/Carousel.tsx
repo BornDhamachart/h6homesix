fa701d788b0a654cbab667497b57a5079ada1417
Dhamachart <dhamachart.c@siamraj.com>
2023-11-07 13:41:20 +0700

    Correct typo JAPANDI HOUSE

--- src/Components/Carousel.tsx
+++ src/Components/Carousel.tsx
@@ -35,9 +35,9 @@ const Carousel : React.FC = () => {
     <div className="h-24 md:h-64 lg:h-64 w-full px-6 md:px-12">
       <Slider {...settings}>
       <ImageLinkComponent path = "/minjihouse" src= "./images/CoverImage/Minji-2.jpeg" name = "MIN-JI HOUSE"/>
-      <ImageLinkComponent path = "/japandihouse" src= "./images/CoverImage/Japandi-5.jpeg" name = "JAPANDI HOUSEE"/>
+      <ImageLinkComponent path = "/japandihouse" src= "./images/CoverImage/Japandi-5.jpeg" name = "JAPANDI HOUSE"/>
       <ImageLinkComponent path = "/minjihouse" src= "./images/CoverImage/Minji-3.jpeg" name = "MIN-JI HOUSE"/>
-      <ImageLinkComponent path = "/japandihouse" src= "./images/CoverImage/Japandi-2.jpeg" name = "JAPANDI HOUSEE"/>
+      <ImageLinkComponent path = "/japandihouse" src= "./images/CoverImage/Japandi-2.jpeg" name = "JAPANDI HOUSE"/>
       </Slider>
     </div>
   );
